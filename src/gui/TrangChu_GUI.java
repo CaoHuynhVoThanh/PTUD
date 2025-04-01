154d5b379d5f023f14888f1209c6caed850e7c4d
Thanh <thanhcaovn23@gmail.com>
2025-04-01 16:05:43 +0700

    sua lai nhieu phan trang chu

--- src/gui/TrangChu_GUI.java
+++ src/gui/TrangChu_GUI.java
@@ -33,6 +33,8 @@ import javax.swing.JTabbedPane;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 import java.awt.SystemColor;
+import javax.swing.JScrollBar;
+import javax.swing.border.LineBorder;
 
 public class TrangChu_GUI extends JFrame {
 
@@ -150,10 +152,10 @@ public class TrangChu_GUI extends JFrame {
 		JMenuItem mi_TrangChu = new JMenuItem("              TRANG CHỦ");
 		mi_TrangChu.setBackground(new Color(255, 153, 0));
 		mi_TrangChu.setSelected(true);
-		mi_TrangChu.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_TrangChu.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_TrangChu.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_TrangChu.setForeground(new Color(255, 255, 255));
-		mi_TrangChu.setBounds(20, 77, 291, 61);
+		mi_TrangChu.setBounds(20, 48, 291, 61);
 		panel.add(mi_TrangChu);
 		
 		JButton btnNewButton = new JButton("ĐĂNG XUẤT");
@@ -167,110 +169,172 @@ public class TrangChu_GUI extends JFrame {
 		mi_DatBan.setSelected(true);
 		mi_DatBan.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_DatBan.setForeground(Color.WHITE);
-		mi_DatBan.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_DatBan.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_DatBan.setBackground(new Color(255, 153, 0));
-		mi_DatBan.setBounds(20, 138, 291, 61);
+		mi_DatBan.setBounds(20, 109, 291, 61);
 		panel.add(mi_DatBan);
 		
 		JMenuItem mi_NhanBan = new JMenuItem("              NHẬN BÀN");
 		mi_NhanBan.setSelected(true);
 		mi_NhanBan.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_NhanBan.setForeground(Color.WHITE);
-		mi_NhanBan.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_NhanBan.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_NhanBan.setBackground(new Color(255, 153, 0));
-		mi_NhanBan.setBounds(20, 200, 291, 61);
+		mi_NhanBan.setBounds(20, 171, 291, 61);
 		panel.add(mi_NhanBan);
 		
 		JMenuItem mi_GoiMon = new JMenuItem("              GỌI MÓN");
 		mi_GoiMon.setSelected(true);
 		mi_GoiMon.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_GoiMon.setForeground(Color.WHITE);
-		mi_GoiMon.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_GoiMon.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_GoiMon.setBackground(new Color(255, 153, 0));
-		mi_GoiMon.setBounds(20, 262, 291, 61);
+		mi_GoiMon.setBounds(20, 233, 291, 61);
 		panel.add(mi_GoiMon);
 		
 		JMenuItem mi_ThanhToan = new JMenuItem("              THANH TOÁN");
 		mi_ThanhToan.setSelected(true);
 		mi_ThanhToan.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_ThanhToan.setForeground(Color.WHITE);
-		mi_ThanhToan.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_ThanhToan.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_ThanhToan.setBackground(new Color(255, 153, 0));
-		mi_ThanhToan.setBounds(20, 322, 291, 61);
+		mi_ThanhToan.setBounds(20, 293, 291, 61);
 		panel.add(mi_ThanhToan);
 		
 		JMenuItem mi_LichSu = new JMenuItem("              LỊCH SỬ");
 		mi_LichSu.setSelected(true);
 		mi_LichSu.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_LichSu.setForeground(Color.WHITE);
-		mi_LichSu.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_LichSu.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_LichSu.setBackground(new Color(255, 153, 0));
-		mi_LichSu.setBounds(20, 382, 291, 61);
+		mi_LichSu.setBounds(20, 353, 291, 61);
 		panel.add(mi_LichSu);
 		
 		JMenuItem mi_ThongKe = new JMenuItem("              THỐNG KÊ");
 		mi_ThongKe.setSelected(true);
 		mi_ThongKe.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_ThongKe.setForeground(Color.WHITE);
-		mi_ThongKe.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_ThongKe.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_ThongKe.setBackground(new Color(255, 153, 0));
-		mi_ThongKe.setBounds(20, 498, 291, 61);
+		mi_ThongKe.setBounds(20, 478, 291, 61);
 		panel.add(mi_ThongKe);
 		
 		JMenuItem mi_QuanLy = new JMenuItem("              QUẢN LÝ");
 		mi_QuanLy.setSelected(true);
 		mi_QuanLy.setHorizontalAlignment(SwingConstants.LEFT);
 		mi_QuanLy.setForeground(Color.WHITE);
-		mi_QuanLy.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		mi_QuanLy.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		mi_QuanLy.setBackground(new Color(255, 153, 0));
-		mi_QuanLy.setBounds(20, 439, 291, 61);
+		mi_QuanLy.setBounds(20, 417, 291, 61);
 		panel.add(mi_QuanLy);
 		
-		JPanel contentsPanel = new JPanel();
-		contentsPanel.setBackground(new Color(255, 255, 255));
-		contentsPanel.setBounds(309, 146, 1204, 660);
-		contentPane.add(contentsPanel);
-		contentsPanel.setLayout(null);
+		JMenuItem mi_ThongKe_1 = new JMenuItem("              TRỢ GIÚP");
+		mi_ThongKe_1.setSelected(true);
+		mi_ThongKe_1.setHorizontalAlignment(SwingConstants.LEFT);
+		mi_ThongKe_1.setForeground(Color.WHITE);
+		mi_ThongKe_1.setFont(new Font("Segoe UI", Font.BOLD, 20));
+		mi_ThongKe_1.setBackground(new Color(255, 153, 0));
+		mi_ThongKe_1.setBounds(20, 541, 291, 61);
+		panel.add(mi_ThongKe_1);
+		
+		JPanel panel_trangchu = new JPanel();
+		panel_trangchu.setBounds(285, 133, 1254, 704);
+		contentPane.add(panel_trangchu);
+		panel_trangchu.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(255, 255, 255));
+		panel_2.setBounds(21, 22, 1209, 253);
+		panel_trangchu.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(new Color(255, 153, 51));
+		panel_5.setBounds(76, 48, 288, 112);
+		panel_2.add(panel_5);
+		
+		JPanel panel_5_1 = new JPanel();
+		panel_5_1.setBackground(new Color(102, 102, 102));
+		panel_5_1.setBounds(840, 48, 288, 112);
+		panel_2.add(panel_5_1);
+		
+		JPanel panel_5_2 = new JPanel();
+		panel_5_2.setBackground(new Color(255, 204, 51));
+		panel_5_2.setBounds(459, 48, 288, 112);
+		panel_2.add(panel_5_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("Thông tin trong ngày");
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(10, 10, 233, 28);
+		panel_2.add(lblNewLabel_1);
 		
 		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(SystemColor.windowBorder);
-		panel_3.setBounds(52, 48, 343, 127);
-		contentsPanel.add(panel_3);
+		panel_3.setBackground(new Color(255, 255, 255));
+		panel_3.setBounds(21, 305, 575, 373);
+		panel_trangchu.add(panel_3);
+		panel_3.setLayout(null);
 		
-		JPanel panel_3_1 = new JPanel();
-		panel_3_1.setBackground(SystemColor.windowBorder);
-		panel_3_1.setBounds(815, 48, 343, 127);
-		contentsPanel.add(panel_3_1);
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		panel_6.setBackground(new Color(255, 255, 255));
+		panel_6.setBounds(50, 89, 212, 237);
+		panel_3.add(panel_6);
+		panel_6.setLayout(null);
 		
-		JPanel panel_3_2 = new JPanel();
-		panel_3_2.setBackground(SystemColor.windowBorder);
-		panel_3_2.setBounds(435, 48, 343, 127);
-		contentsPanel.add(panel_3_2);
+		JLabel lblNewLabel_2 = new JLabel("ĐƠN ĐẶT BÀN");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_2.setBounds(47, 10, 122, 24);
+		panel_6.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Chờ nhận:");
+		lblNewLabel_3.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_3.setBounds(22, 65, 79, 24);
+		panel_6.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Chờ nhận:");
+		lblNewLabel_3_1.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_3_1.setBounds(22, 99, 79, 24);
+		panel_6.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_2 = new JLabel("Chờ nhận:");
+		lblNewLabel_3_2.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel_3_2.setBounds(22, 133, 79, 24);
+		panel_6.add(lblNewLabel_3_2);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Trạng thái đơn và bàn");
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 16));
+		lblNewLabel_1_1.setBounds(10, 10, 233, 28);
+		panel_3.add(lblNewLabel_1_1);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(255, 255, 255));
+		panel_4.setBounds(618, 305, 612, 373);
+		panel_trangchu.add(panel_4);
+		panel_4.setLayout(null);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Thống kê món phổ biến");
+		lblNewLabel_1_1_1.setFont(new Font("Arial", Font.BOLD, 16));
+		lblNewLabel_1_1_1.setBounds(10, 10, 233, 28);
+		panel_4.add(lblNewLabel_1_1_1);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(684, 266, 474, 347);
-		contentsPanel.add(scrollPane);
+		scrollPane.setBounds(10, 98, 592, 232);
+		panel_4.add(scrollPane);
 		
 		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setRowSelectionAllowed(false);
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
 			},
 			new String[] {
-				"T\u00EAn m\u00F3n", "Lo\u1EA1i m\u00F3n", "\u0110\u01A1n gi\u00E1", "S\u1ED1 l\u01B0\u1EE3ng b\u00E1n"
+				"T\u00EAn m\u00F3n", "Lo\u1EA1i m\u00F3n", "\u0110\u01A1n gi\u00E1", "L\u01B0\u1EE3t d\u00F9ng"
 			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, Object.class, Object.class, Object.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-		});
-		scrollPane.setViewportView(table);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(93, 319, 202, 289);
-		contentsPanel.add(panel_2);
+		));
+		scrollPane.setColumnHeaderView(table);
 	}
 }
